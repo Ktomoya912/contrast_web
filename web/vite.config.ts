9ae6a51d0380ae53d1f808882196a2c9762542ce
ktomoya912 <295133i@gs.kochi-tech.ac.jp>
2025-12-24 15:34:16 +0900

    Fix deploy base path and add model weight

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/contrast_alphazero/', // GitHub Pages repo name
+  base: '/contrast_web/', // GitHub Pages repo name
 })
